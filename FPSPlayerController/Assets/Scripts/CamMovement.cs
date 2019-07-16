9abc28d494ee9032745289166a5074e93c901e24
Twan <twanhelleman@gmail.com>
2019-07-16 15:30:08 +0200

    the initial FPS controller
    -mouse camera movement
    -walk in the direction the camera is facing
    -crouching
    -jumping

---
+++ FPSPlayerController/Assets/Scripts/CamMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour
+{
+    [SerializeField, Range(0, 25)]
+    float turningSpeed;
+
+    float verticalCamRotation, horizontalCamRotation, xRotation;
+
+    void Start()
+    {
+        //hide the cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        //cam horizontal rotation
+        horizontalCamRotation = Input.GetAxis("Mouse X") * turningSpeed;
+        transform.rotation *= Quaternion.Euler(0, horizontalCamRotation, 0);
+
+        //cam vertical rotation
+        verticalCamRotation = Input.GetAxis("Mouse Y") * (turningSpeed / 2); //half the verical rotation speed for a better result
+        verticalCamRotation *= -1; //inverse the rotation
+        xRotation += verticalCamRotation; //add to rotation
+        Clamp(ref xRotation, -70, 70); //clamp the rotation
+        Vector3 rot = new Vector3(xRotation, transform.rotation.eulerAngles.y, 0);
+        transform.localRotation = Quaternion.Euler(rot);
+    }
+
+    void Clamp(ref float value, float min, float max)
+    {
+        if (value < min)
+        {
+            value = min;
+        }
+        else if (value > max)
+        {
+            value = max;
+        }
+    }
+}
---
+++ FPSPlayerController/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float walkSpeed, runSpeed, jumpPower;
+
+    [SerializeField]
+    Collider normalCollider, crounchCollider;
+
+    [SerializeField]
+    Transform cam;
+
+    Vector3 camInitPos;
+    float movementSpeed;
+    Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        camInitPos = cam.transform.localPosition;
+    }
+
+    void Update()
+    {
+        //input
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+
+        //velocity
+        Vector3 velocity = cam.transform.TransformDirection(movement);
+        velocity.y = 0;
+
+        //set speed
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            movementSpeed = runSpeed;
+        }
+        else
+        {
+            movementSpeed = walkSpeed;
+        }
+
+        //jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(transform.up * jumpPower
+, ForceMode.Impulse);
+        }
+
+
+        //crouch
+        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+        {
+            normalCollider.enabled = false;
+            crounchCollider.enabled = true;
+            cam.transform.localPosition = new Vector3(camInitPos.x, -camInitPos.y / 4, camInitPos.z);//deviding the y axis by four and taking the minus of that gives the best results
+            movementSpeed /= 2;
+        }
+        else
+        {
+            normalCollider.enabled = true;
+            crounchCollider.enabled = false;
+            cam.transform.localPosition = camInitPos;
+        }
+
+        //movement
+        rigidbody.position += velocity * (movementSpeed * Time.deltaTime);
+
+    }
+}
